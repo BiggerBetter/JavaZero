5258b0e1a54e2e121b139829668e452fd8771e5e
BiggerBetter <jiong23@qq.com>
2022-08-06 16:53:34 +0800

    深度解压缩文件夹

--- src/Wanting/UnZipFolder.java
+++ src/Wanting/UnZipFolder.java
@@ -1,35 +1,76 @@
 package Wanting;
-import java.io.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.zip.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+/**
+ * 1.target a folder find inside zip files
+ * 2.目标zip文件如果是个文件夹就新建个文件夹，如果是文件就解压在当前目录
+ * 3.if unzip process got a new folder, continue to unzip the new folder
+ */
 public class UnZipFolder {
-    public static void main(String[] args) throws Exception {
-        List<String> folderList = new ArrayList<>();
-        folderList.add("/Users/Jenius/Desktop/unzip2/");
+    static List<String> folderList = new ArrayList<>();
 
-        String srcPath = "/Users/Jenius/Desktop/unzip2/中文.pptx.zip";
-        String dest = "/Users/Jenius/Desktop/unzip2/中文.pptx";
-        decompress(srcPath,dest);
+    public static void main(String[] args) throws Exception {
+
+        String target = "/Users/Jenius/Desktop/UEC2/";
+        File sf = new File(target);
+        if(sf.isDirectory()){
+            folderList.add(sf.getPath());
+        }else {
+            decompress(sf.getPath());
+        }
+
+        while (!folderList.isEmpty()) {
+            String tmpfolderPath = folderList.remove(0);
+            if (!"".equals(tmpfolderPath)) {
+                File file = new File(tmpfolderPath);
+                File[] files = file.listFiles();
+                assert files != null;
+                for (File f : files) {
+                    if (f.isDirectory()) {
+                        folderList.add(f.getPath());
+                    }else if(f.getName().endsWith(".zip")){
+                        decompress(f.getPath());
+                    }
+                }
+
+            }
+        }
+        listAllFileDeleteBlank(target);
+        deleteAllZipFile(target);
     }
+
     /**
      * 解压文件/文件夹
+     * srcPath：带文件名的路径
+     * dest：目标文件夹，不带文件名
      */
-    public static void decompress(String srcPath, String dest) throws Exception {
+    public static void decompress(String srcPath) throws Exception {
         File file = new File(srcPath);
+
+        String dest = file.getParent();
+
         if (!file.exists()) {
             throw new RuntimeException(srcPath + "所指文件不存在");
         }
+
         ZipFile zf = new ZipFile(file);
-        Enumeration entries = zf.entries();
+        Enumeration<? extends ZipEntry> entries = zf.entries();
         ZipEntry entry = null;
         while (entries.hasMoreElements()) {
             entry = (ZipEntry) entries.nextElement();
             System.out.println("解压" + entry.getName());
             if (entry.isDirectory()) {
                 String dirPath = dest + File.separator + entry.getName();
+                //add unzipped folder in to folderList
+                folderList.add(dirPath);
                 File dir = new File(dirPath);
                 dir.mkdirs();
             } else {
@@ -39,26 +80,81 @@ public class UnZipFolder {
                     //String dirs = FileUtils.getParentPath(f);
                     String dirs = f.getParent();
                     File parentDir = new File(dirs);
-                    parentDir.mkdirs();
+                    parentDir.mkdirs();//兼容已经存在的情况
                 }
                 f.createNewFile();
                 // 将压缩文件内容写入到这个文件中
-                InputStream is = zf.getInputStream(entry);
-                FileOutputStream fos = new FileOutputStream(f);
+                InputStream inStream = zf.getInputStream(entry);
+                FileOutputStream outStream = new FileOutputStream(f);
                 int count;
                 byte[] buf = new byte[8192];
-                while ((count = is.read(buf)) != -1) {
-                    fos.write(buf, 0, count);
+                while ((count = inStream.read(buf)) != -1) {
+                    outStream.write(buf, 0, count);
                 }
-                is.close();
-                fos.close();
+                inStream.close();
+                outStream.close();
             }
         }
     }
 
 
+    public static void listAllFileDeleteBlank(String path) {
+        File f = new File(path);
+        if(!f.isDirectory()){
+            return;
+        }
+        File[] files = f.listFiles();
+        for (File file : files) {
+            System.out.println(file);
+            if (file.isDirectory()) {
+                if (file.getName().equals("__MACOSX")) {
+                    deleteFile(file.getPath());
+                    continue;
+                }
+                listAllFileDeleteBlank(file.getPath());
+            }
+        }
+    }
+
+    public static void deleteAllZipFile(String path) {
+        File f = new File(path);
+        if(!f.isDirectory()){
+            return;
+        }
+        File[] files = f.listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                deleteAllZipFile(file.getPath());
+            }else {
+                if (file.getName().endsWith(".zip")) {
+                    deleteFile(file.getPath());
+                }
+            }
+        }
+    }
+
     /**
-     * 清理所有的__MACOSX文件夹
+     * 递归删除子文件
+     *
+     * @param path 文件路径
      */
+    public static void deleteFile(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File[] temp = file.listFiles(); //获取该文件夹下的所有文件
+                assert temp != null;
+                for (File value : temp) {
+                    deleteFile(value.getAbsolutePath());
+                }
+            } else {
+                //删除子文件
+                file.delete();
+            }
+            //删除子文件
+            file.delete();
+        }
+    }
 }
 
